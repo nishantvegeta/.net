e6ba1183b93a80a47be03e77fca4711fb6a20b5f
nishant kumpakha <94302514+nishantvegeta@users.noreply.github.com>
2023-04-02 07:59:06 +0545

    Add files via upload

---
+++ ConsoleApp3 polymorphism/ConsoleApp3 polymorphism/Overloading.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3_polymorphism
+{
+    internal class Animal
+    {
+            public void WhatIEat()
+            {
+                Console.WriteLine("I eat grass. ");
+            }
+            public void WhatIEat(string food) 
+            { 
+                Console.WriteLine("I eat "+ food);
+            }
+            public void WhatIEat(string food1,  string food2) 
+            { 
+                Console.WriteLine("I eat "+ food1 + " and "+ food2);
+            }
+    }
+
+}
---
+++ ConsoleApp3 polymorphism/ConsoleApp3 polymorphism/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3_polymorphism
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal cow = new Animal();
+            cow.WhatIEat();
+            cow.WhatIEat("momo");
+            cow.WhatIEat("momo", "water");
+            Console.ReadKey();  
+        }
+    }
+}
