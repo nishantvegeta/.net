7d3dcfd9d9fe41dbcae103b0b172c253b3b04374
nishant kumpakha <94302514+nishantvegeta@users.noreply.github.com>
2023-04-03 08:09:28 +0545

    Add files via upload

--- generic collection/generic collection/Program.cs
+++ generic collection/generic collection/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -34,16 +35,24 @@ namespace generic_collection
         }*/
 
             //generic collection
-            List<int> RollNo = new List<int>();
-            RollNo.Add(101);
-            RollNo.Add(102);
-            RollNo.Add(103);
-            foreach (int item in RollNo) 
-            {
-                Console.WriteLine("Roll no. is "+ item);
-            }
-            Console.ReadKey();
+            /* List<int> RollNo = new List<int>();
+             RollNo.Add(101);
+             RollNo.Add(102);
+             RollNo.Add(103);
+             foreach (int item in RollNo) 
+             {
+                 Console.WriteLine("Roll no. is "+ item);
+             }
+             Console.ReadKey();
+         }*/
+
+            FileStream file = new FileStream("nish.dat",FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            //file.WriteByte((byte)10);
+            Console.WriteLine(file.ReadByte()+" ");
+            file.Close();
+            Console.ReadKey();       
         }
+        
         
     }
 }
