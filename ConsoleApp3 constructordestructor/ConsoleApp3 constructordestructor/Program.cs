21b3074fada39588fc71dfa15fc640d6031dee8c
nishant kumpakha <94302514+nishantvegeta@users.noreply.github.com>
2023-04-04 07:42:06 +0545

    Add files via upload

---
+++ ConsoleApp3 constructordestructor/ConsoleApp3 constructordestructor/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3_constructordestructor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal cow = new Animal();
+            cow.WhatIEat();
+            Animal dog = new Animal("Dog");
+            dog.WhatIEat("Bone");
+            Console.ReadKey();
+        }
+    }
+    public class Animal
+    {
+        public Animal()
+        {
+            Console.WriteLine("I am animal. ");
+        }
+        public void WhatIEat()
+        {
+            Console.WriteLine("I eat grass.");
+        }
+        public Animal(String name)
+        {
+            Console.WriteLine("I am " + name);
+        }
+        public void WhatIEat(string food)
+        {
+            Console.WriteLine("I eat " + food);
+        }
+        ~Animal() 
+        {
+
+        }
+    }
+
+}
---
+++ ConsoleApp5 delegate/ConsoleApp5 delegate/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5_delegate
+{
+    internal class Program
+    {
+        public delegate void WhatIEat();
+        public delegate void WhatIEats(string food);
+        static void Main(string[] args)
+        {
+            Animal cow = new Animal();
+            Animal dog = new Animal();
+            WhatIEat eat = new WhatIEat(cow.WhatIeat);
+            WhatIEats eats = new WhatIEats(dog.WhatIeat);
+        eat();
+        eats("Bone");
+            Console.ReadKey();  
+        }
+    }
+    public class Animal
+    {
+        public void WhatIeat() 
+        {
+            Console.WriteLine("I eat grass. ");
+        }
+        public void WhatIeat(string food) 
+        {
+            Console.WriteLine("I eat "+ food);
+        }
+    }
+}
